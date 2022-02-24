3ff7d42e54d1751706d38a952008d6e43b971a64
Lidia Setubal <lidiassbarbosa@gmail.com>
2022-02-24 17:45:47 -0300

    final commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+
+export default ({ black }) => {
+  return (
+    <header className={black ? "black" : ""}>
+      <div className="header--logo">
+        <a href="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            alt="Netflix"
+          ></img>
+        </a>
+      </div>
+      <div className="header--user">
+        <a href="/">
+          <img
+            src="https://ia803404.us.archive.org/31/items/profiles_202104/default.png"
+            alt="Usuário"
+          />
+        </a>
+      </div>
+    </header>
+  );
+};
